6623b515cb12ea411b5eacc590983ca1b93a5ee7
RenanGAS <renansakaoki@gmail.com>
2023-05-23 15:29:49 -0300

    feat: test somethings

---
+++ trab2/main.c
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+static int width;
+static int height;
+
+static void display(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+
+
+    // GPETO
+    
+    glViewport(0, 0, width/2, height/2); // Esquerda baixo
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-3.0, 3.0, -3.0, 3.0,
+            1, 50); // distancia da camera pro plano near, e pro far
+    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0, 0.0, 2.5,  // x, y, z -  posição da câmera
+              0.0, 0.0, 0.0,  // x, y, z -  pra onde a câmera aponta
+              0.0, 1.0, 0.0); // x, y, z -  vetor view up
+    
+    glPushMatrix();
+    glutWireTeapot(1);
+    glPopMatrix();
+
+    // END GPETO
+    
+
+    glViewport(width/2, 0, width/2, height/2); // Direta baixo
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0, 0.0, 2.5,
+              0.0, 0.0, 0.0,
+              0.0, 1.0, 0.0);
+    glutWireTeapot(1);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-3.0, 3.0, -3.0, 3.0, 
+            1, 50);
+
+    glViewport(0, height/2, width/2, height/2); // Esquerda cima
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0, 2.5, 0.0,
+              0.0, 0.0, 0.0,
+              0.0, 0.0, -1.0);
+    glutWireTeapot(1);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-3.0, 3.0, -3.0, 3.0, 1, 50);
+
+    glViewport(width/2, height/2, width/2, height/2); // Direita cima
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(2.5, 0.0, 0.0,
+              0.0, 0.0, 0.0,
+              0.0, 1.0, 0.0);
+    glutWireTeapot(1);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-3.0, 3.0, -3.0, 3.0, 1, 50);
+
+    glFlush();
+}
+
+static void reshape(int w, int h) {
+    width = w;
+    height = h;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_FLAT);
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
+    return EXIT_SUCCESS;
+}
